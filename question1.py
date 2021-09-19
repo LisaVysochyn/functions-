df3e0461491da88e7a74cfb6f1a96549a5e436ce
LisaVysochyn <90222844+LisaVysochyn@users.noreply.github.com>
2021-09-19 19:44:30 +0300

    Update question1.py

--- question1.py
+++ question1.py
@@ -7,6 +7,6 @@ There are 2.54 centimeters in an inch.'''
 length_in_centimeters = float(input('Enter a length in centimeters: '))
 
 if length_in_centimeters>=0:
-    print('Length in inch:', length_in_centimeters*2.54)
+    print('Length in inch:', round(length_in_centimeters/2.54))
 else:
     print('The entry is invalid!')
